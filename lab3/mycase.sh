e88f6e813e2dc0e29eb7394671390f8d47f80adc
Shehab eldeen Adullah <shehab.abdullah12@gmail.com>
2022-10-22 15:01:58 +0200

    update mycase.sh to work in bash not ksh

--- lab3/mycase.sh
+++ lab3/mycase.sh
@@ -1,10 +1,10 @@
-#! /bin/ksh
+#! /bin/bash
 
 case $1 in
-+([a-z]) ) echo lower ;;
-+([A-Z]) ) echo upper ;;
-+([0-9]) ) echo number ;;
-+([0-9a-zA-Z]) ) echo mix ;;
+^[a-z]+$ ) echo lower ;;
+^[A-Z]+$ ) echo upper ;;
+^[0-9]+$ ) echo number ;;
+^[0-9a-zA-Z]+$ ) echo mix ;;
 * ) echo other ;;
 
 esac
